6f505f7ec2a03b8399d5c2428a2c1f53c51e7424
Adelya Fattakhova <AdIFattakhova@stud.kpfu.ru>
2022-12-15 12:23:36 +0300

    new

--- users/views.py
+++ users/views.py
@@ -33,3 +33,6 @@ def register(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/register.html', context)
+
+def profile(request):
+    return render(request,'users/profile.html')
